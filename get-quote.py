a911dbca74fcd2d215600bb9acb91b5ccad59456
David Greyling <64464191+coronath3beer@users.noreply.github.com>
2022-03-14 23:45:30 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,10 @@
-def primary():
+#def primary():
     #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[13])
-  if __name__== "__main__":
-    primary()
+  #print(quotes[13])
+ # if __name__== "__main__":
+    #primary()
